90391fe221a4a8b6c6c82df94bf1edce264eadc6
shuta13 <hollystarsun@gmail.com>
2020-01-12 16:39:53 +0900

    fix: refactored insertKeyframes by optional chaining

--- src/insertKeyframes.ts
+++ src/insertKeyframes.ts
@@ -2,6 +2,5 @@ export const keyframes = (rules: string): void | string => {
   const styleElem = document.createElement("style");
   document.head.appendChild(styleElem);
   let styleSheet = styleElem.sheet as CSSStyleSheet | null; // eslint-disable-line
-  if (styleSheet === null) return "err";
-  else styleSheet.insertRule(rules, styleSheet.cssRules.length);
+  styleSheet?.insertRule(rules, styleSheet.cssRules.length);
 };
